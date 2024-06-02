dcf4b8f34dbade5e2d645c6fb5e15df041a927d8
deepaksaikillada <kdsai2003@gmail.com>
2024-06-02 22:31:17 +0530

    deepak has pushed

--- access_modifiers/access_properties_in_class.cpp
+++ access_modifiers/access_properties_in_class.cpp
@@ -16,14 +16,17 @@ class hero{
     char level;             
 
     //behaviours/functions/methods
-
+    void attack(){
+        cout<<health*level<<endl;
+    }
 };
 
 int main(){
 
     //creation of object h1 with hero class/data type
     hero h1;
-    cout<<"health is : "<<h1.health<<endl;  // by default the (data members)/(properties) are private in the class; so we can't access it outside the class
+    cout<<"health is : "<<h1.health<<endl;  // by default the (data members)/(properties)/members/functions are private in the class; so we can't access it outside the class
+    cout<<"Attack power is : "<<h1.attack<<endl; 
     /*
     error: 'int hero::health' is private within this context
      cout<<"health"<<h1.health<<endl;  //
